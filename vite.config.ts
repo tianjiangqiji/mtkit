5a4244e4d70c9eda6f67b6b1227be60a32dc40ba
guohub.top <41369069+guohub8080@users.noreply.github.com>
2025-03-29 13:53:09 +0800

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     extensions: [".ts", ".tsx", ".js", "jsx"]
   },
   base: './',
-  publicDir:"public",
+  publicDir:"./public",
   build: {
     outDir: "docs",
     minify: isProduction,
