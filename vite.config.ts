3ba9e3a15fbd447945137fff324ca43a3878de18
guohub.top <41369069+guohub8080@users.noreply.github.com>
2025-03-29 13:42:51 +0800

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -21,9 +21,9 @@ export default defineConfig({
     extensions: [".ts", ".tsx", ".js", "jsx"]
   },
   base: './',
-  publicDir:"",
+  publicDir:"public",
   build: {
-    outDir: "docs",
+    outDir: "",
     minify: isProduction,
     assetsInlineLimit: 4096, // 小于此阈值的导入或引用资源将内联为 base64 编码
     rollupOptions: {
