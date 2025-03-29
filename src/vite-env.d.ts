d14d30d116108af280d02f74fdb2d6e2ee8005dc
guohub.top <41369069+guohub8080@users.noreply.github.com>
2025-03-29 15:27:25 +0800

    bug fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,3 +3,9 @@ declare module '*.tsx'
 declare module '*.jsx'
 declare module '*.js'
 declare module '*.ts'
+
+declare module "*.svg" {
+  import React from "react";
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
