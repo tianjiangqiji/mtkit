849ec91f1236df176f433e886003933d230d9dde
guohub8080 <guo2018@88.com>
2025-04-27 00:50:11 +0800

    bug fix

--- src/assets/datas/VITE_ENVS.ts
+++ src/assets/datas/VITE_ENVS.ts
@@ -1,5 +1,5 @@
 export const base = () => {
-	if (import.meta.env.VITE_GITHUB_PAGES === 'true') return '/mtkit'
+	if (import.meta.env.VITE_GITHUB_PAGES === 'true') return '/mtkit/'
 	if (import.meta.env.VITE_UNIAPP === 'true') return ''
 	return '/';
 }
